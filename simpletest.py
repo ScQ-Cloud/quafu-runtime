78867aa409814b06ddb2d36e955bd76ee4da2b4a
beizha <beizhansl@163.com>
2023-03-28 12:34:20 +0800

    simpletest.py

--- simpletest.py
+++ simpletest.py
@@ -32,16 +32,28 @@ class TestAPI():
         response = job.result()
         print(response)
 
+
     @staticmethod
-    def TestJob_wait():
-        account = Account("testapitoken")
-        service = RuntimeService(account)
-        job = service.run(program_id="784376be3f8b40309be7e1e9ab7c7404", backend="py_simu", inputs="zxxx")
-        print(job.job_id())
-        print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-        job.result(wait=True)
+    def parallelTest(num):
+        import multiprocessing
+        nums = num
+        plist = []
+        for i in range(nums):
+            p = multiprocessing.Process(target=TestJob_wait)
+            plist.append(p)
+            p.start()
+        for p in plist:
+            p.join()
+
+def TestJob_wait():
+    account = Account("testapitoken")
+    service = RuntimeService(account)
+    job = service.run(program_id="784376be3f8b40309be7e1e9ab7c7404", backend="py_simu", inputs="zxxx")
+    print(job.job_id())
+    print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+    job.result(wait=True)
 
 if __name__ == '__main__':
     print(os.getcwd())
-    TestAPI.TestJob_wait()
     # TestAPI.TestJob_wait()
+    TestAPI.parallelTest(num=5)
