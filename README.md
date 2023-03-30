bf890876b48abd168a176c69887ef056b4fe00e3
Zhaoyilunnn <dilved1014@gmail.com>
2023-03-30 05:44:52 +0000

    Refine README

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # quafu_runtime_demo
+
+## How to run simpletest
+
+1. `TestUpload`
+```
+metadata = {"name": "<specify the name here>", "backend": "testbackend"}
+
+```
+
+2. `TestRun`
+
+```
+job = service.run(program_id="<Specify the program_id (the output of 1)>", ...)
+```
--- simpletest.py
+++ simpletest.py
@@ -11,21 +11,21 @@ class TestAPI():
     def TestUpload():
         account = Account("testapitoken")
         service = RuntimeService(account)
-        metadata = {"name": "testname1", "backend": "testbackend"}
+        metadata = {"name": "testname2", "backend": "testbackend"}
         program_id = service.upload_program(data='program/hello.py', metadata=metadata)
         print(program_id)
     @staticmethod
     def TestRun():
         account = Account("testapitoken")
         service = RuntimeService(account)
-        job = service.run(program_id="784376be3f8b40309be7e1e9ab7c7404", backend="py_simu", inputs="zsl")
+        job = service.run(program_id="409c55020cda4eedbae341fe316c1970", backend="py_simu", inputs="zsl")
         print(job.job_id())
 
     @staticmethod
     def TestJob_nowait():
         account = Account("testapitoken")
         service = RuntimeService(account)
-        job = service.run(program_id="784376be3f8b40309be7e1e9ab7c7404", backend="py_simu", inputs="zxxx")
+        job = service.run(program_id="409c55020cda4eedbae341fe316c1970", backend="py_simu", inputs="zxxx")
         print(job.job_id())
         print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
         # sleep(5)
@@ -56,4 +56,4 @@ def TestJob_wait():
 if __name__ == '__main__':
     print(os.getcwd())
     # TestAPI.TestJob_wait()
-    TestAPI.parallelTest(num=5)
+    TestAPI.TestRun()
