694d30ff8ac816a0beaa1c83274b1f374bb65973
beizha <beizhansl@163.com>
2023-05-29 14:48:56 +0800

    Add update program method

--- quafu_runtime/program/program.py
+++ quafu_runtime/program/program.py
@@ -9,10 +9,27 @@ class RuntimeProgram:
         self.data = None
         self.max_cost_time = None
 
+    def update(self, program: dict):
+        if 'program_id' in program:
+            self.program_id = program['program_id']
+        if 'data' in program:
+            # response['data'] = from_base64_string(response['data']).decode("utf-8")
+            self.data = program['data']
+        if 'description' in program:
+            self.description = program['description']
+        if 'name' in program:
+            self.name = program['name']
+        if 'is_public' in program:
+            self.is_public = program['is_public']
+        if 'backend' in program:
+            self.backend = program['backend']
+        if 'cost' in program:
+            self.max_cost_time = program['cost']
+
     def __str__(self):
         return f"<Program id:{self.program_id}, name:{self.name}\n" \
                f"is_public:{self.is_public}, backend:{self.backend}\n" \
                f"description:{self.description}, max_cost_time:{self.max_cost_time}\n" \
-               f"data:{self.data}>"
+               f"data:\n{self.data}>"
 
 
